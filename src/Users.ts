13e79150043666115c7dd7d4086243c7aed18c3d
Leandrho <leandro.coder01@gmail.com>
2025-02-19 12:05:32 -0300

    add Users class

---
+++ src/Users.ts
@@ -0,0 +1,39 @@
+import fs from 'fs';
+
+export class Users{
+    private userSet :Set<string>;
+
+    constructor(){
+        this.userSet=new Set<string>();
+    }
+
+    public loadUserFromFile(filePath:string){
+        if(!fs.existsSync('users/Logueos.csv'))
+            throw new Error(`File does not exist: ${filePath}`);
+
+        const users = fs.readFileSync(filePath,'utf-8').split('\n');
+        for(let i = 0; i<users.length; i++){
+            const [user, ] = users[i].split(';');
+            this.userSet.add(user);
+        }
+    }
+    public has(user: string){
+        return this.userSet.has(user);
+    }
+}
+// let finalUser :string[] = [];
+
+// const ginkgoUsers = fs.readFileSync('output-Login.csv','utf-8').split('\n');
+// for(let i = 0; i<ginkgoUsers.length; i++){
+//     const [user, ] = ginkgoUsers[i].split(';');
+//     if(userBiloSet.has(user))
+//         finalUser.push(user);
+// }
+
+// fs.writeFileSync('final-login-users-cross.csv', '');
+// finalUser.forEach((user)=>{
+//     if(!user )
+//         return;
+//     fs.appendFileSync('final-login-users-cross.csv',`${user}\n`);
+// })
+   
